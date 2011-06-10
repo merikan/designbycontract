425ebfc94a15d975bf684736ec4d0cb2b6f12f36
john.a.dobie <john.a.dobie@59fd130d-417b-22cf-ff8a-b815a6ea62b7>
2011-06-10 13:23:48 +0000

    initial draft

---
+++ trunk/multi-module-sonar/module1/src/integrationtest/java/com/jpmorgan/ib/IntegrationTest.java
@@ -0,0 +1,15 @@
+package com.jpmorgan.ib;
+
+import org.junit.Test;
+
+import com.dbc.JavaClass;
+
+public class IntegrationTest {
+
+	@Test
+	public void emptyTest() throws Exception 
+	{
+
+	}
+
+}
---
+++ trunk/multi-module-sonar/module2/src/integrationtest/java/com/dbc/IntegrationTestModule2.java
@@ -0,0 +1,15 @@
+package com.dbc;
+
+import org.junit.Test;
+
+import com.dbc.JavaClass;
+
+public class IntegrationTestModule2 {
+
+	@Test
+	public void emptyTest() throws Exception 
+	{
+		JavaClass jc = new JavaClass();
+	}
+
+}
