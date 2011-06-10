e3dd0c19e71ca7b929a72977e34e661616954135
john.a.dobie <john.a.dobie@59fd130d-417b-22cf-ff8a-b815a6ea62b7>
2011-06-10 15:01:09 +0000

    Update tests

---
+++ multi-module-sonar/module2/src/integrationtest/java/com/dbc/Module2IntegrationTest.java
@@ -0,0 +1,14 @@
+package com.dbc;
+
+import org.junit.Test;
+
+public class Module2IntegrationTest {
+
+	@Test
+	public void simpleTest() throws Exception 
+	{
+		Module2JavaClass jc = new Module2JavaClass();
+		jc.setName("name");
+	}
+
+}
---
+++ multi-module-sonar/module2/src/test/java/com/dbc/Module2UnitTest.java
@@ -0,0 +1,13 @@
+package com.dbc;
+
+import org.junit.Test;
+
+
+public class Module2UnitTest {
+
+	@Test
+	public void simpleTest() throws Exception {
+		Module2JavaClass javaClass = new Module2JavaClass();
+		javaClass.setVersion(1);
+	}
+}
