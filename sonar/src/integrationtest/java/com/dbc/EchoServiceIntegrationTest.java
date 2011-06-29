1458c7059776a05c423ecd0db78cefeb11c89063
john.a.dobie <john.a.dobie@gmail.com>
2011-06-29 15:37:16 +0000

    restore test

---
+++ sonar/src/integrationtest/java/com/dbc/EchoServiceIntegrationTest.java
@@ -0,0 +1,19 @@
+package com.dbc;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EchoServiceIntegrationTest {
+
+	private static final String TEST_MESSAGE = "Integration Test";
+	
+	@Test
+	public void emptyTest() throws Exception 
+	{
+		EchoService echoService = new EchoService();
+		echoService.setVersion(1);
+		String message = echoService.echo(TEST_MESSAGE);
+		assertEquals(message, TEST_MESSAGE); 
+	}
+
+}
