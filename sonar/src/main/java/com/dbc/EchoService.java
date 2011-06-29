a754271f34bed33eaff125c97a6f523f9d065090
john.a.dobie <john.a.dobie@gmail.com>
2011-06-29 15:31:56 +0000

    Add extra fields to demonstrate the different code coverage between unit and integration tests

--- sonar/src/main/java/com/dbc/EchoService.java
+++ sonar/src/main/java/com/dbc/EchoService.java
@@ -1,9 +1,28 @@
 package com.dbc;
 
 public class EchoService {
+	
+	private int version = 0;
+	private String name = "";
 
 	public String echo(String message)
 	{
 		return message;
+	}
+	
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
