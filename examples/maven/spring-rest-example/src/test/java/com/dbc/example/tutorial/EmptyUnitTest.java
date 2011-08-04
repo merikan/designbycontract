d8fb159192e6fe4f4f70988a89a7f60ed03efc5e
john.a.dobie <john.a.dobie@gmail.com>
2011-08-04 13:36:32 +0000

    initial import

---
+++ examples/maven/spring-rest-example/src/test/java/com/dbc/example/tutorial/EmptyUnitTest.java
@@ -0,0 +1,18 @@
+package com.dbc.example.tutorial;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author W370984
+ * A simple test to show the unit tests being run in a separate phase in the build.
+ */
+public class EmptyUnitTest {
+
+	@Test
+	public void testNothing() throws Exception
+	{
+		
+	}
+	
+}
