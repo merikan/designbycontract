a2765fe4f21c67d02dc7e63557677efa955eb140
john.a.dobie@gmail.com <john.a.dobie@gmail.com@59fd130d-417b-22cf-ff8a-b815a6ea62b7>
2011-09-05 16:16:26 +0000

    Change tests to use xstream to deserialise before doing assertions

--- trunk/examples/maven/spring-rest-example/src/integrationtest/java/com/dbc/service/TradeServiceTest.java
+++ trunk/examples/maven/spring-rest-example/src/integrationtest/java/com/dbc/service/TradeServiceTest.java
@@ -4,6 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
 
+import com.dbc.model.Trade;
+import com.thoughtworks.xstream.XStream;
+
 public class TradeServiceTest {
 
 	public void testGetTradeFromRestService() throws Exception {
@@ -11,17 +14,34 @@ public class TradeServiceTest {
 				.getForObject(
 						"http://localhost:8080/search/trade/{reference}",
 						String.class, "1");
+		
+		Trade t = getTradeFromXml(result);
+		System.out.println(t.getId());
 		Assert.assertNotNull(result);
 		System.out.println(result);
 	}
 	
 	@Test
 	public void testCreateTradeFromRestService() throws Exception {
+		long id = 1L;
+		String trade = createTrade(1L);
+		Trade t = getTradeFromXml(trade);
+		Assert.assertEquals(t.getId(), id);
+	}
+
+	private Trade getTradeFromXml(String trade) {
+		XStream xstream = new XStream();
+		xstream.processAnnotations(Trade.class);
+		Trade t = (Trade)xstream.fromXML(trade);
+		return t;
+	}	
+	
+	private String createTrade(Long id)
+	{
 		String result = new RestTemplate()
-				.getForObject(
-						"http://localhost:8080/create/trade/{id}",
-						String.class, "1");
-		Assert.assertNotNull(result);
-		System.out.println(result);
+		.getForObject(
+				"http://localhost:8080/create/trade/{id}",
+				String.class, "1");
+		return result;
 	}
 }
--- trunk/examples/maven/spring-rest-example/src/main/java/com/dbc/model/Trade.java
+++ trunk/examples/maven/spring-rest-example/src/main/java/com/dbc/model/Trade.java
@@ -9,25 +9,35 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
 @Entity
 public class Trade {
 
-	@Id	
-	private Long id;
+	@Id
+	private long id;
 	private String reference;
 	private String description;
 
-	public Trade(Long id)
-	{
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Trade(long id) {
 		this.id = id;
 	}
 
 	public String getReference() {
 		return reference;
 	}
+
 	public void setReference(String reference) {
 		this.reference = reference;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
