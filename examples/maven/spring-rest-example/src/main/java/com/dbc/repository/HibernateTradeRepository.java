cb104de76c2142f18d3fdcc203f4a0354085db97
john.a.dobie <john.a.dobie@gmail.com>
2012-01-13 11:04:07 +0000

    JIRA-1 Example fix

--- examples/maven/spring-rest-example/src/main/java/com/dbc/repository/HibernateTradeRepository.java
+++ examples/maven/spring-rest-example/src/main/java/com/dbc/repository/HibernateTradeRepository.java
@@ -5,7 +5,7 @@ import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import com.dbc.model.Trade;
 
 public class HibernateTradeRepository  extends HibernateDaoSupport implements TradeRepository{
-
+ 
 	@Override
 	public Trade getTradeByReference(String reference) {
 
