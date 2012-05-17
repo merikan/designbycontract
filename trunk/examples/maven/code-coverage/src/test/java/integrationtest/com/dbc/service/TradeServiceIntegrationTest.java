926f77e8c60274f72c54376bb41d44116dd871be
john.a.dobie@gmail.com <john.a.dobie@gmail.com@59fd130d-417b-22cf-ff8a-b815a6ea62b7>
2012-05-17 15:01:39 +0000

    Tidied up the integration test

--- trunk/examples/maven/code-coverage/src/test/java/integrationtest/com/dbc/service/TradeServiceIntegrationTest.java
+++ trunk/examples/maven/code-coverage/src/test/java/integrationtest/com/dbc/service/TradeServiceIntegrationTest.java
@@ -4,51 +4,29 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.springframework.web.client.RestTemplate;
 
 import com.dbc.model.Trade;
 import com.dbc.test.annotation.type.IntegrationTest;
-import com.thoughtworks.xstream.XStream;
 
 @Category(IntegrationTest.class)
 public class TradeServiceIntegrationTest {
 	
-	private String endpoint = "http://localhost:8081/code-coverage";
-	
+	private TestHelper helper = new TestHelper();
+
 	@Test
-	public void testCreateTradeFromRestService() throws Exception {
-		long id = (long) (Math.random()*100000);
-		String tradeXml = createTrade(id);
-		Trade t = getTradeFromXml(tradeXml);
+	public void testCreateTradeFromRestService() throws Exception 
+	{
+		long id = helper.getRandomId();
+		Trade t = helper.createTrade(id);
 		assertEquals(t.getId(), id);
 	}
 
 	@Test
-	public void testGetTradeFromRestService() throws Exception {
-		long id = (long) (Math.random()*100000);
-		createTrade(id);
-		String tradeXml = new RestTemplate()
-				.getForObject(endpoint + "/find/trade/{id}",
-						String.class, id);
-		
-		System.out.println(tradeXml);
-		Trade trade = getTradeFromXml(tradeXml);
+	public void testFindTrade() throws Exception {		
+		long id = helper.getRandomId();
+		Trade t = helper.createTrade(id);
+		Trade trade = helper.findTrade(t.getId());		
 		assertEquals(trade.getId(), id);
 	}
 
-	private Trade getTradeFromXml(String trade) {
-		XStream xstream = new XStream();
-		xstream.processAnnotations(Trade.class);
-		Trade t = (Trade)xstream.fromXML(trade);
-		return t;
-	}	
-	
-	private String createTrade(Long id)
-	{
-		String tradeXml = new RestTemplate()
-		.getForObject(
-				endpoint + "/create/trade/{id}",
-				String.class, id);
-		return tradeXml;
-	}
 }
