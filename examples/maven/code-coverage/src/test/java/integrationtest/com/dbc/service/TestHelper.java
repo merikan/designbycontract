9fdf507963444a68d54f049e5fbac199ad3c7d3e
john.a.dobie <john.a.dobie@gmail.com>
2012-05-17 15:01:39 +0000

    Tidied up the integration test

---
+++ examples/maven/code-coverage/src/test/java/integrationtest/com/dbc/service/TestHelper.java
@@ -0,0 +1,35 @@
+package com.dbc.service;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.dbc.model.Trade;
+import com.thoughtworks.xstream.XStream;
+
+public class TestHelper {
+
+	private String ENDPOINT = "http://localhost:8081/code-coverage";
+
+	public Trade createTrade(Long id) {
+		String tradeXml = new RestTemplate().getForObject(ENDPOINT
+				+ "/create/trade/{id}", String.class, id);
+		return getTradeFromXml(tradeXml);
+	}
+
+	public Trade findTrade(long id) {
+		String tradeXml = new RestTemplate().getForObject(ENDPOINT
+				+ "/find/trade/{id}", String.class, id);
+		return getTradeFromXml(tradeXml);
+	}
+
+	public long getRandomId() {
+		long id = (long) (Math.random() * 100000);
+		return id;
+	}
+
+	public Trade getTradeFromXml(String trade) {
+		XStream xstream = new XStream();
+		xstream.processAnnotations(Trade.class);
+		Trade t = (Trade) xstream.fromXML(trade);
+		return t;
+	}
+}
