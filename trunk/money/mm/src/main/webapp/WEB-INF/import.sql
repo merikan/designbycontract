e0a17eeec4b258bf3d098881c1435064003422ef
john.a.dobie@gmail.com <john.a.dobie@gmail.com@59fd130d-417b-22cf-ff8a-b815a6ea62b7>
2012-10-21 09:58:57 +0000

    add categories

---
+++ trunk/money/mm/src/main/webapp/WEB-INF/import.sql
@@ -0,0 +1,35 @@
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('1', 'Grocery', 'Grocery');
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('2', 'Cash Withdrawal', 'Cash Withdrawl');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('3', 'Groceries', 'Groceries');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('4', 'Takeaways', 'Takeaways');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('5', 'Off License', 'Off License');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('6', 'Cleaner', 'Cleaner');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('7', 'Car', 'Car');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('8', 'Clothes', 'Clothes');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('9', 'Clubs And Passes', 'Clubs And Passes');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('10', 'Birthdays', 'Birthdays');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('11', 'Christmas', 'Christmas');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('12', 'Home Improvement', 'Home Improvement');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('13', 'Garden', 'Garden');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('14', 'Emergencies', 'Emergencies');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('15', 'Big Purchases', 'Big Purchases');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('16', 'One Off\'s', 'One Off\'s');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('17', 'John', 'John');
+
+INSERT INTO `mm`.`category` (`category_id`, `description`, `name`) VALUES ('18', 'Emma', 'Emma');
+
