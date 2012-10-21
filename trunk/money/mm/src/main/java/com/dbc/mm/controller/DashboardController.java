26bb30909d5b7804d29e58ff2bf1a1277f93fede
john.a.dobie@gmail.com <john.a.dobie@gmail.com@59fd130d-417b-22cf-ff8a-b815a6ea62b7>
2012-10-21 10:02:24 +0000

    add category dropdown

--- trunk/money/mm/src/main/java/com/dbc/mm/controller/DashboardController.java
+++ trunk/money/mm/src/main/java/com/dbc/mm/controller/DashboardController.java
@@ -4,14 +4,14 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.dbc.mm.form.DashboardForm;
-import com.dbc.mm.model.Transaction;
+import com.dbc.mm.service.category.CategoryService;
 import com.dbc.mm.service.transaction.TransactionService;
 
 @Controller
@@ -22,19 +22,24 @@ public class DashboardController extends AbstractApplicationController {
 
 	@Autowired
 	TransactionService transactionService;
+	
+	@Autowired
+	CategoryService categoryService;
+
 
 	@RequestMapping(value = "/view", method = RequestMethod.GET)
 	public ModelAndView view() {
 		logger.debug("**********");
 		
 		DashboardForm form = new DashboardForm();
+		form.setAllCategories(categoryService.findAll());
 		form.setAllTransactions(transactionService.findAll());
 		return new ModelAndView("/dashboard/view_dashboard", "form", form);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, value="/movie")  
-	public @ResponseBody String getMovie(Model model){
-		logger.debug("**********");
+	@RequestMapping(method=RequestMethod.POST, value="/updateTransaction")  
+	public @ResponseBody String getMovie(@RequestParam("transactionId") String transactionId, @RequestParam("categoryId") String categoryId, Model model){
+		logger.debug("**********" + transactionId + " " + categoryId);
 	    return "value";
 	}
 	
--- trunk/money/mm/src/main/java/com/dbc/mm/form/DashboardForm.java
+++ trunk/money/mm/src/main/java/com/dbc/mm/form/DashboardForm.java
@@ -2,12 +2,22 @@ package com.dbc.mm.form;
 
 import java.util.List;
 
+import com.dbc.mm.model.Category;
 import com.dbc.mm.model.Transaction;
 
 
 public class DashboardForm {
 	
 	private List<Transaction> allTransactions;
+	public List<Category> getAllCategories() {
+		return allCategories;
+	}
+
+	public void setAllCategories(List<Category> allCategories) {
+		this.allCategories = allCategories;
+	}
+
+	private List<Category> allCategories;
 
 	public List<Transaction> getAllTransactions() {
 		return allTransactions;
